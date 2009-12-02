771219247231f87ee91a3fe6395af0b2a2820b5c
Jonathan Stott <jonathan.stott@gmail.com>
2009-12-02 03:36:55 +0000

    Refactoring site action.

--- drhyde.rb
+++ drhyde.rb
@@ -1,6 +1,8 @@
-#!/usr/bin/ruby
-# drhyde.rb
-# Jonathan D. Stott <jonathan.stott@gmail.com>
+# Sinatra app to display a jekyll site.
+# TODO:
+# * implement posts
+# * Add Cache-Control
+# * test on heroku
 
 require 'sinatra'
 require 'jekyll'
@@ -8,14 +10,8 @@ require 'net/http'
 
 # reopen albino
 class Albino
-  def initialize(target, lexer = :text, format = :html)
-    @target  = File.exists?(target) ? File.read(target) : target rescue target
-    @options = { :l => lexer, :f => format, :O => 'encoding=utf-8' }
-  end
-
   def colorize(options={})
     html  = Net::HTTP.post_form(URI.parse('http://pygments.appspot.com/'), {'lang'=> @options[:l], 'code' => @target}).body
-    puts html
     # Work around an RDiscount bug: http://gist.github.com/97682
     html.to_s.sub(%r{</pre></div>\Z}, "</pre>\n</div>")
   end
@@ -23,7 +19,7 @@ class Albino
 end
 
 set :app, __FILE__
-set :jekyll_config, proc { Jekyll.configuration('source' => Sinatra::Application.root, 'server' => false, 'markdown'     => 'rdiscount') }
+set :jekyll_config, proc { Jekyll.configuration('source' => Sinatra::Application.root, 'server' => false) }
 set :jekyll, proc {
   j = Jekyll::Site.new(Sinatra::Application.jekyll_config)
   j.read_layouts
@@ -46,44 +42,44 @@ get %r{^.*$} do
   root = File.expand_path(options.root)
   path = File.expand_path(root + unescape(request.path_info))
   pass if path[0, root.length] != root
-  unescaped_path = path.dup
-  unescaped_path[0, root.length + 1] = ""
 
-  # deal with root dir or dirs without slashes.
-  if File.directory?(path) or unescaped_path == ""
-    unescaped_path << "/"
+
+  # deal with dirs
+  if File.directory?(path)
+    # add a slash if we don't have one on the end.
+    path << "/" if path[-1, 1] != "/"
+
+    # look for the index
+    path << "index.html"
   end
 
-  if unescaped_path[-1, 1] == "/"
-    dir = unescaped_path
-    base = "index"
-    ext = ".html"
-  else
+  # split things out for jekyll
+  dir = path.dup
+  dir[0, root.length + 1] = ""
+  filename = File.basename(dir)
+  dir = File.dirname(dir)
 
-    # find the file extension and base
-    dir = File.dirname(unescaped_path)
-    ext = File.extname(unescaped_path)
-    base = File.basename(unescaped_path, ".*")
-  end
-#  p dir, base, ext
 
-#  p File.join(dir,(base + '.markdown')), File.exist?(File.join(dir,(base + '.markdown')))
+  # if it's a file, serve it.
+  if File.file?(path)
+    content_type(media_type(File.extname(path)) || 'application/octet-stream' )
+    last_modified(File.mtime(path))
+    first3 = File.open(path) { |fd| fd.read(3) }
 
-  case ext
-  when "", ".htm", ".html"
-    if File.file?(path)
-      send_file(path, :disposition => nil)
-    elsif File.exist?(file = File.join(dir,(base + '.markdown')))
-      content_type "text/html"
-#      last_modified File.mtime(file)
-      j = Jekyll::Page.new(options.jekyll, root, dir, base + '.markdown')
-      p options.jekyll.layouts, options.jekyll.site_payload
+    if first3 == "---"
+      # page has a yaml header, process as page
+      j = Jekyll::Page.new(options.jekyll, root, dir, filename)
       j.render(options.jekyll.layouts, options.jekyll.site_payload)
       j.output
     else
-      not_found
+      send_file path, :disposition => nil
     end
-  else 
-    send_file(path)
+  elsif File.file?(path.sub(/html$/,'markdown')) # look for a markdown page
+    content_type "text/html", :charset => 'utf-8'
+    j = Jekyll::Page.new(options.jekyll, root, dir, filename.sub(/html$/,'markdown'))
+    j.render(options.jekyll.layouts, options.jekyll.site_payload)
+    j.output
+  else
+    not_found
   end
 end
