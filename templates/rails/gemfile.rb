f4bdbc2b412f6e6a0ed9307b5777ed9d16eb56b2
Dan Kubb <dan.kubb@gmail.com>
2010-09-10 08:43:33 -0700

    Bumped gem dependencies in Gemfile template

--- templates/rails/gemfile.rb
+++ templates/rails/gemfile.rb
@@ -12,8 +12,8 @@ create_file 'Gemfile' do
 <<-GEMFILE
 source 'http://rubygems.org'
 
-RAILS_VERSION = '~> 3.0.0.rc'
-DM_VERSION    = '~> 1.0.0'
+RAILS_VERSION = '~> 3.0.0'
+DM_VERSION    = '~> 1.0.2'
 
 gem 'activesupport',      RAILS_VERSION, :require => 'active_support'
 gem 'actionpack',         RAILS_VERSION, :require => 'action_pack'
