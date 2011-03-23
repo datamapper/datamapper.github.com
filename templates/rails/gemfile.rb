fd4f3a86e4fc62769c38d8115af698d3275184f1
Ian MacLeod <ian@nevir.net>
2011-03-23 12:23:19 +0800

    Bumping the templates up to 1.1.0

--- templates/rails/gemfile.rb
+++ templates/rails/gemfile.rb
@@ -12,15 +12,15 @@ create_file 'Gemfile' do
 <<-GEMFILE
 source 'http://rubygems.org'
 
-RAILS_VERSION = '~> 3.0.1'
-DM_VERSION    = '~> 1.0.2'
+RAILS_VERSION = '~> 3.0.4'
+DM_VERSION    = '~> 1.1.0'
 
 gem 'activesupport',      RAILS_VERSION, :require => 'active_support'
 gem 'actionpack',         RAILS_VERSION, :require => 'action_pack'
 gem 'actionmailer',       RAILS_VERSION, :require => 'action_mailer'
 gem 'railties',           RAILS_VERSION, :require => 'rails'
 
-gem 'dm-rails',          '~> 1.0.4'
+gem 'dm-rails',          '~> 1.1.0'
 gem 'dm-#{database}-adapter', DM_VERSION
 
 # You can use any of the other available database adapters.
