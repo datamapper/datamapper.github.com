3226534db6bb78c7637b4f058b73c5dc37e9b88e
Dan Kubb <dan.kubb@gmail.com>
2012-03-31 22:07:50 -0700

    Remove JRuby patch now that the bug is fixed in a stable release

--- templates/rails/application.rb
+++ templates/rails/application.rb
@@ -12,22 +12,6 @@ inject_into_class 'app/controllers/application_controller.rb',
                   'ApplicationController',
                   "  use Rails::DataMapper::Middleware::IdentityMap\n"
 
-initializer 'jruby_monkey_patch.rb', <<-CODE
-if RUBY_PLATFORM =~ /java/
-  # ignore the anchor to allow this to work with jruby:
-  # http://jira.codehaus.org/browse/JRUBY-4649
-  class Rack::Mount::Strexp
-
-    class << self
-      alias :compile_old :compile
-      def compile(str, requirements, separators, anchor)
-        self.compile_old(str, requirements, separators)
-      end
-    end
-  end
-end
-CODE
-
 say ''
 say '---------------------------------------------------------------------------'
 say 'Have a look at the dm-rails README:   http://github.com/datamapper/dm-rails'
